5d88ea9e85bb5dba417e8188b26ada2393cce708
Dan Barrett <dan@theblahman.net>
2014-03-05 14:28:31 +0800

    Added error handling for the form

--- app/views/form.blade.php
+++ app/views/form.blade.php
@@ -6,6 +6,16 @@ $data = $dataString;
 
 {{ Form::open ( [ 'action' => 'LastfmController@formProcessor' ] ) }}
 
+	<ul class="errors">
+	
+		@foreach ( $errors->all () as $message )
+		
+			<li>{{ $message }}</li>
+		
+		@endforeach
+	
+	</ul>
+
 	{{ Form::label ( 'username' , 'Username' ) }}
 	
 	{{ Form::text ( 'user' , Input::get ( 'user' ) , [ 'id' => 'username' ] ) }}
